cb0d2fd7565d978329307671b4f60bb57c63033f
Joel Wraith <joel.wraith@gmail.com>
2021-02-10 13:37:36 +0000

    Refactor code to allow for more reusability in the future

--- mountain_car.py
+++ mountain_car.py
@@ -4,51 +4,72 @@ from gym.wrappers.monitoring.video_recorder import VideoRecorder
 from datetime import datetime
 from agent import Agent
 
-def print_data(action, exploring, current_reward, total_reward, epsilon):
-    os.system('cls')
-    print('Action Taken: {}, {}'.format(action, 'Exploring' if exploring else 'Exploiting'))
-    print('Epsilon: {}'.format(epsilon))
-    print('Current Reward: {}'.format(current_reward))
-    print('Total Reward: {}'.format(total_reward))
+class MountainCar:
+    def __init__(self):
+        self._output_directory = "videos/{}".format(datetime.now().strftime("%d%m%H%M%S"))
+        os.mkdir(self._output_directory)
+        self._env = gym.make('MountainCar-v0')
+        # gym.logger.set_level(gym.logger.DEBUG)
+        self._agent = Agent(self._env)
+        self._n_episodes = 1000
+        self._current_episode = -1
+        self._target_position = 0.5
+        self._action_map = ['Left', 'Nothing', 'Right']
+    
 
+    def _print_data(self, exploring, current_reward):
+        os.system('cls')
+        print('Action Taken: {}, {}'.format(action, 'Exploring' if exploring else 'Exploiting'))
+        print('Epsilon: {}'.format(self._agent.epsilon))
+        print('Current Reward: {}'.format(current_reward))
+        print('Total Reward: {}'.format(self._total_reward))
 
-output_directory = "videos/{}".format(datetime.now().strftime("%d%m%H%M%S"))
-os.mkdir(output_directory)
-env = gym.make('MountainCar-v0')
-# gym.logger.set_level(gym.logger.DEBUG)
-agent = Agent(env)
-n_episodes = 1000
-target_position = 0.5
-
-action_map = ['Left', 'Nothing', 'Right']
-
-for episode_number in range(n_episodes):
-    done = False
-    inital_state = agent.reshape_input(env.reset())
-
-    video_recorder = VideoRecorder(env, "{}/test{}.mp4".format(output_directory, episode_number), enabled=False)
-
-    total_reward = 0
-    state = inital_state
-    while not done:
-        # env.render()
-        video_recorder.capture_frame()
-        action, action_was_random = agent.get_action(state)
-        next_state, reward, done, _ = env.step(action)  
-        if next_state[0] >= target_position:
-            new_reward += 50
+    def run_tick(self):#Is tick the right termonology here?
+        self._env.render()
+        action, action_was_random = self._agent.get_action(self._state)
+        next_state, reward, self._done, _ = self._env.step(action)  
+        if next_state[0] >= self._target_position:
+            new_reward = 50
         else:
-            new_reward = reward * (target_position - next_state[0])
-        total_reward += new_reward
-        # print_data(action_map[action],action_was_random, new_reward, total_reward, agent.epsilon)
-        next_state = agent.reshape_input(next_state)
-        agent.save_to_memory_buffer(state, action, reward, next_state, done)
-        state = next_state
+            new_reward = reward * (self._target_position - next_state[0])
+        self._total_reward += new_reward
+        # print_data(self._action_map[action],action_was_random, new_reward, self.+total_reward, agent.epsilon)
+        next_state = self._agent.reshape_input(next_state)
+        self._agent.save_to_memory_buffer(self._state, action, reward, next_state, self._done)
+        self._state = next_state
 
-        if done:
-            print("Episode {}/{}, Score: {}".format(episode_number, n_episodes, total_reward))
-            video_recorder.close()
+        if self._done:
+            print("Episode {}/{}, Score: {}".format(self._current_episode, self._n_episodes, self._total_reward))
+          # video_recorder.close()
+
+    def close(self):
+        self._env.close()
+
+    def train_model(self):
+        self._agent.train_model()
         
-    agent.train_model()
+    def intialise_episode(self, episode_number):
+        self._current_episode = episode_number
+        self._done = False
+        self._inital_state = self._agent.reshape_input(self._env.reset())
+        self._total_reward = 0
+        self._state = self._inital_state
+        # video_recorder = VideoRecorder(env, "{}/test{}.mp4".format(output_directory, episode_number), enabled=False)
 
-env.close()
+    def get_episodes(self):
+        return self._n_episodes
+
+    def is_done(self):
+        return self._done
+
+
+
+solution = MountainCar()
+
+for episode_number in range(solution.get_episodes()):
+    solution.intialise_episode(episode_number)
+    while not solution.is_done():
+       solution.run_tick()
+    solution.train_model()
+
+solution.close()
