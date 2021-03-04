1052d965bc4e44bddea62ba1642284289bed9934
Joel Wraith <joel.wraith@gmail.com>
2021-03-04 16:13:49 +0000

    Implement profling and new rewards system

--- agent.py
+++ agent.py
@@ -5,17 +5,17 @@ import random
 from collections import deque
 
 class Agent:
-    def __init__(self, enviroment):
+    def __init__(self, environment):
         self.gamma = 0.99
         self.learning_rate = 0.001
 
-        self.epsilon = 0.95
+        self.epsilon = 1
         self.epsilon_decay = 0.995
         self.epsilon_min = 0.01
         self.training_batch_size = 32
 
-        self.state_space_size = enviroment.observation_space.shape[0]
-        self.action_space_size = enviroment.action_space.n
+        self.state_space_size = environment.observation_space.shape[0]
+        self.action_space_size = environment.action_space.n
 
         self.memory_buffer = deque(maxlen=2000)
 
@@ -25,8 +25,8 @@ class Agent:
         self.memory_buffer.append((state, action, reward, next_state, done))
 
     def get_action(self, state):
-        if np.random.rand(0, 1) < self.epsilon:
-            return (np.random.randint(self.action_space_size), True)
+        if random.uniform(0, 1) < self.epsilon:
+            return (random.randrange(self.action_space_size), True)
         else:
             action_values = self.model.predict(np.reshape(state, [1, self.state_space_size]))
             return (np.argmax(action_values), False)
@@ -84,3 +84,72 @@ class Agent:
 
     def reshape_input(self, input):
         return np.reshape(input, [1, self.state_space_size])
+
+class ExampleAgent(Agent):
+    def __init__(self, environment, batch_size=32, memory_size=50000):
+        self.state_space_size = environment.observation_space.shape[0]
+        self.action_space_size = environment.action_space.n
+        self.batch_size = batch_size
+        self.memory = deque(maxlen=memory_size)
+        self.training = 10000  # training after 10000 env steps
+        self.gamma = 0.95  # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+
+    def _build_model(self):
+        model = keras.models.Sequential()
+        model.add(keras.layers.Dense(24, input_dim=self.state_space_size, activation='relu'))
+        model.add(keras.layers.Dense(24, activation='relu'))
+        model.add(keras.layers.Dense(self.action_space_size, activation='linear'))
+        model.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=self.learning_rate))
+        return model
+
+    def save_to_memory_buffer(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def get_action(self, state):
+        if random.uniform(0, 1) < self.epsilon:
+            return (random.randrange(self.action_space_size), True)
+        else:
+            return (np.argmax(self.model.predict(state)[0]), False)
+
+    def train_model(self):
+        # Updates the online network weights after enough data is collected
+        if self.training >= len(self.memory):
+            return
+
+        # Samples a batch from the memory
+        random_batch = random.sample(self.memory, self.batch_size)
+
+        state = np.zeros((self.batch_size, self.state_space_size))
+        next_state = np.zeros((self.batch_size, self.state_space_size))
+        action, reward, done = [], [], []
+
+        for i in range(self.batch_size):
+            state[i] = random_batch[i][0]
+            action.append(random_batch[i][1])
+            reward.append(random_batch[i][2])
+            next_state[i] = random_batch[i][3]
+            done.append(random_batch[i][4])
+
+        # Batch prediction to save compute costs
+        target = self.model.predict(state)
+        target_next = self.model(next_state)
+
+        for i in range(len(random_batch)):
+            if done[i]:
+                target[i][action[i]] = reward[i]
+            else:
+                target[i][action[i]] = reward[i] + self.gamma * (np.amax(target_next[i]))
+
+        self.model.fit(
+            np.array(state),
+            np.array(target),
+            batch_size=self.batch_size,
+            verbose=0
+        )
+
+        self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
--- mountain_car.py
+++ mountain_car.py
@@ -1,13 +1,15 @@
+import cProfile, pstats
 import gym
 import pyglet
 import os
+import io
 from gym.wrappers.monitoring.video_recorder import VideoRecorder
 from datetime import datetime
-from agent import Agent
-from mountain_car_env import MountainCarEnv
+from agent import Agent, ExampleAgent
 import keyboard
 from csv_writer import CsvWriter
 import time
+import cProfile
 class MountainCar:
     def __init__(self):
         self._output_directory = "data/{}".format(datetime.now().strftime("%d%m%H%M%S"))
@@ -36,13 +38,20 @@ class MountainCar:
         action, action_was_random = self._agent.get_action(self._state)
         self._current_episode_actions.append(action)
         next_state, reward, self._done , _ = self._env.step(action)  
+
+        if next_state[1] > self._state[0][1] >= 0 and next_state[1] >= 0:
+            reward = 20
+
+        if next_state[1] < self._state[0][1] <= 0 and next_state[1] <= 0:
+            reward = 20
+
         if next_state[0] >= self._target_position:
-            new_reward = 200
-            self._successful = True
+            reward += 10000
         else:
-            new_reward = reward
-        self._episode_total_reward += new_reward
-        self._episode_max_reward = max(self._episode_max_reward, new_reward)
+            reward -= 25
+
+        self._episode_total_reward += reward
+        self._episode_max_reward = max(self._episode_max_reward, reward)
         # print_data(self._action_map[action],action_was_random, new_reward, self.+total_reward, agent.epsilon)
         next_state = self._agent.reshape_input(next_state)
         self._agent.save_to_memory_buffer(self._state, action, reward, next_state, self._done)
@@ -106,19 +115,38 @@ class HandControl(MountainCar):
         if state[0] >= self._target_position:
             self._successful = True
 
+# https://qxf2.com/blog/saving-cprofile-stats-to-a-csv-file/
+def export_profiling_results(profiler, file_name):
+    result = io.StringIO()
+    pstats.Stats(profiler,stream=result).sort_stats("cumtime").print_stats()
+    result=result.getvalue()
+    # chop the string into a csv-like buffer
+    result='ncalls'+result.split('ncalls')[-1]
+    result='\n'.join([','.join(line.rstrip().split(None,5)) for line in result.split('\n')])
+    # save it to disk
+    
+    with open(file_name, 'w+') as f:
+        #f=open(result.rsplit('.')[0]+'.csv','w')
+        f.write(result)
+        f.close()
+
 
 solution = MountainCar()
-
 start_time = time.time()
+profiler = cProfile.Profile()
+gym.logger.set_level(gym.logger.DEBUG)
 
 for i in range(solution.get_episode_count()):
     episode_number = i + 1
     solution.intialise_episode(episode_number)
+    profiler.enable()
     while not solution.current_episode_done():
        solution.run_tick()
-
     solution.train_model()
+    profiler.disable()
     
+    export_profiling_results(profiler, '{}/episode{}.csv'.format(solution._output_directory, episode_number))
+
     if solution.current_episode_successful() or episode_number % 50 == 0:
         solution.replay_episode()
     done_time = time.time()
